8dd1d94bc49eb5dd1ba39eb2723bb85c4505e5d3
Alagbada123 <114651605+Alagbada123@users.noreply.github.com>
2025-05-29 23:36:49 +0200

    Update __init__.py

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -8,4 +8,7 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.users import *
 
+from models.user import User # Ensuring User is explicitly available before use
 User.load_from_file()
+
+from api.v1.views.session_auth import * # Added as per Req 8
